72c5ccd081e886a3cd54809dad23b1d48c3f20fb
xmxm00 <cmcm0012@gmail.com>
2023-04-07 14:46:24 +0900

    initial commit

---
+++ src/main/java/com/numble/webnovel/WebnovelApplication.java
@@ -0,0 +1,13 @@
+package com.numble.webnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebnovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebnovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numble/webnovel/WebnovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.webnovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebnovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
